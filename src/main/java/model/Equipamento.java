a48e5c4da6574b99f27bcb6768d9e9264ac5febe
joao.mendes <joaopedrosao@gmail.com>
2024-01-10 06:13:27 -0300

    initial commit.

---
+++ src/main/java/model/Equipamento.java
@@ -0,0 +1,5 @@
+package model;
+
+public class Equipamento {
+    private Setor setor;
+}
---
+++ src/main/java/model/Setor.java
@@ -0,0 +1,6 @@
+package model;
+
+public class Setor {
+    private String nome;
+    private int andar;
+}
