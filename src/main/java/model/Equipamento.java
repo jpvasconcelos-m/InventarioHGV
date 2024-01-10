60384c343ce8a574051253905e2d773f4dd27a04
joaop <joaopedrosao@gmail.com>
2024-01-10 16:10:14 -0300

    adicionados novos atributos a classe Equipamento

--- src/main/java/model/Equipamento.java
+++ src/main/java/model/Equipamento.java
@@ -3,4 +3,10 @@ package model;
 public class Equipamento {
     private Setor setor;
     private TipoEquipamento tipoEquipamento;
+    private String hostname;
+    private int tag;
+    private String ip;
+    private String modelo;
+    private String serialOuTomb;
+    private String origem;
 }
