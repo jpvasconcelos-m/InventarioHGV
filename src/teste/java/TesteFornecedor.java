a3481ec4d3a9f13e09a087f7e46f392a9b13947d
joao.mendes <joaopedrosao@gmail.com>
2024-01-11 23:47:41 -0300

    adicionado teste de acesso do hashset fornecedores por outras classes

--- src/teste/java/TesteFornecedor.java
+++ src/teste/java/TesteFornecedor.java
@@ -1,10 +1,18 @@
 import model.Fornecedor;
 
+ import java.io.ObjectStreamException;
 import java.util.HashSet;
+import java.util.stream.Collectors;
 
 public class TesteFornecedor {
     public static void main(String[] args) {
-        Fornecedor.fornecedores.add(new Fornecedor());
+        Fornecedor.fornecedores.add(new Fornecedor("Rgraph", "email@email.com", 99832821));
+        Fornecedor.fornecedores.stream().forEach(f -> {
+            System.out.println(
+                    "Nome: " + f.getNome() + " Email: " + f.getEmail() + " Telefone: " + f.getTelefone());
+        });
 
     }
+
+
 }
