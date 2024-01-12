7d9d99fd47e5ec17418a9a65cb2bf4861d45215b
joao.mendes <joaopedrosao@gmail.com>
2024-01-11 22:08:00 -0300

    criada classe para testes para a classe Fornecedor usando um metodo main.

---
+++ src/teste/java/TesteFornecedor.java
@@ -0,0 +1,10 @@
+import model.Fornecedor;
+
+import java.util.HashSet;
+
+public class TesteFornecedor {
+    public static void main(String[] args) {
+        Fornecedor.fornecedores.add(new Fornecedor());
+
+    }
+}
