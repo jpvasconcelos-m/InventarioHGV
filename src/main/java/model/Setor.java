30659c3e6b4b5af910f62d4ae54dbfea74319f9e
joao.mendes <joaopedrosao@gmail.com>
2024-01-16 02:08:22 -0300

    Adicionados construtores e anotações para JPA

--- src/main/java/model/Setor.java
+++ src/main/java/model/Setor.java
@@ -1,7 +1,43 @@
 package model;
 
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Setor {
     private String nome;
     private int andar;
     private int ramal;
+
+    public Setor(String nome, int andar, int ramal) {
+        this.nome = nome;
+        this.andar = andar;
+        this.ramal = ramal;
+    }
+
+    public Setor() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAndar() {
+        return andar;
+    }
+
+    public void setAndar(int andar) {
+        this.andar = andar;
+    }
+
+    public int getRamal() {
+        return ramal;
+    }
+
+    public void setRamal(int ramal) {
+        this.ramal = ramal;
+    }
 }
