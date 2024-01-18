80ccdd0741cf50e0f7fe6e1042304342aa22cf9a
joao.mendes <joaopedrosao@gmail.com>
2024-01-17 22:59:57 -0300

    Teste amplificado.

--- src/main/java/testes/TesteCriarEquipamentoDB.java
+++ src/main/java/testes/TesteCriarEquipamentoDB.java
@@ -5,20 +5,36 @@ import model.Equipamento;
 import model.Fornecedor;
 import model.Setor;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class TesteCriarEquipamentoDB {
+
+    static EntityManagerFactory emf = Persistence.createEntityManagerFactory("InventarioHGV");
+    static EntityManager em = emf.createEntityManager();
     public static void main(String[] args) {
-        DAO<Equipamento> eqpDAO = new DAO<>(Equipamento.class);
+
         Fornecedor fornecedor = new Fornecedor("INFORP","INFORP@INFORP.COM",40028922);
         Setor setor = new Setor("EMERG",3,40028922);
-
         Equipamento eqp = new Equipamento(
                 setor,"GVCOMPUT",123456,"172.16.1.1","LENOVO","TOP","GVLOC1234",fornecedor);
 
+       DAO<Object> dao = new DAO<>();
 
-     eqpDAO.openTransaction();
-     eqpDAO.include(eqp);
-     eqpDAO.closeTransaction();
-        System.out.println("Operação concluída!");
-    }
+       dao.openTransaction();
+       dao.include(fornecedor);
+       dao.include(setor);
+       dao.include(eqp);
+       dao.closeTransaction();
+       dao.close();
+
+
+
+
 
 }
+
+}
+
