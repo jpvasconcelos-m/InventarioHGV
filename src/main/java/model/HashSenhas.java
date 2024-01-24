fa274f838de2ccc207605825deac262745722e02
joao.mendes <joaopedrosao@gmail.com>
2024-01-23 22:08:34 -0300

    adicionados métodos para geração de salts e hashcode.

---
+++ src/main/java/model/HashSenhas.java
@@ -0,0 +1,17 @@
+package model;
+
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class HashSenhas {
+    private static BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public String generateSalt(){
+        return BCrypt.gensalt();
+    }
+
+    public String hashPassword(String passwordPlusSalt){
+        return passwordEncoder.encode(passwordPlusSalt);
+    }
+
+}
