d22f65ef754c1e87f1608742c6d73eb022333082
joaop <joaopedrosao@gmail.com>
2024-01-24 20:12:48 -0300

    criado teste para testar classe de hash e salt de senhas.

---
+++ src/main/java/testes/TesteHashSenhas.java
@@ -0,0 +1,17 @@
+package testes;
+
+import model.HashSenhas;
+
+public class TesteHashSenhas {
+   static HashSenhas hs = new HashSenhas();
+
+    public static void main(String[] args) {
+      String salt =   hs.generateSalt();
+      String senha=  "senha1234";
+
+      String senhaComSalt = senha + salt;
+        System.out.println("Senha com salt: " + senhaComSalt);
+      String senhaComHashESalt = hs.hashPassword(senhaComSalt);
+        System.out.println("Senha com hash e salt:" + senhaComHashESalt);
+    }
+}
