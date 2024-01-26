2187d59801d601ad210dde5085f8744bcdbbf786
joaop <joaopedrosao@gmail.com>
2024-01-26 17:17:20 -0300

    Criado teste para Criação de usuarios no DB.

---
+++ src/main/java/testes/TesteCriarUsuarioDB.java
@@ -0,0 +1,29 @@
+package testes;
+
+import infra.DAO;
+import model.HashSenhas;
+import model.Usuario;
+
+import java.util.Scanner;
+
+public class TesteCriarUsuarioDB {
+    public static void main(String[] args) {
+        Usuario usuario = new Usuario();
+        HashSenhas hs = new HashSenhas();
+        Scanner sc = new Scanner(System.in);
+        DAO<Object> dao = new DAO<>();
+
+
+        usuario.setLogin("Loginteste123");
+        usuario.setNome("Pedro Alvares de Cabral");
+        usuario.setHashSenha(hs.hashPassword(sc.nextLine() + usuario.getSalt())); //Digitar a senha.
+
+        System.out.println(usuario.getHashSenha());
+
+        dao.atomicInclude(usuario);
+
+
+
+    }
+
+}
