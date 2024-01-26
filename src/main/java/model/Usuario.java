3744787d6185e0fa0ab79cbd59cb1f3d1517eaf2
joao.mendes <joaopedrosao@gmail.com>
2024-01-26 07:01:55 -0300

    Adicionados construtores e atributo de Id e salt.

--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -1,14 +1,58 @@
 package model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity(name = "usuarios")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nomeUsuario;
     private String hashSenha;
+    private String salt;
 
     private String nome;
-    private String senha;
+
+    public Usuario(String nomeUsuario, String hashSenha, String salt, String nome) {
+        this.nomeUsuario = nomeUsuario;
+        this.hashSenha = hashSenha;
+        this.salt = salt;
+        this.nome = nome;
+    }
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getHashSenha() {
+        return hashSenha;
+    }
+
+    public void setHashSenha(String hashSenha) {
+        this.hashSenha = hashSenha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
