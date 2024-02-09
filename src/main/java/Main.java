e054de80abc3a31bf112bb09acdae1da1c1001c8
joaop <joaopedrosao@gmail.com>
2024-02-09 15:16:30 -0300

    adicionado codigo de execução de tela de login à classe Main.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,19 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import view.TelaLogin;
+
 public class Main {
+
+    static TelaLogin tl = new TelaLogin();
+
+    public static void main(String[] args) {
+        try {
+            tl.start(new Stage());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
 }
