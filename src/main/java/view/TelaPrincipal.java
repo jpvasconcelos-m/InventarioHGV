2b32230ba5fbcbe64e4836b105f600d896f68e55
joaop <joaopedrosao@gmail.com>
2024-02-09 19:25:19 -0300

    Adicionada classe de tela principal

---
+++ src/main/java/view/TelaPrincipal.java
@@ -0,0 +1,17 @@
+package view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TelaPrincipal extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
