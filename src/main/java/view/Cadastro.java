40961800ae1d7a59b76721e4ccd562b02df28a20
joao.mendes <joaopedrosao@gmail.com>
2024-02-21 01:15:13 -0300

    criada classe controller para o fxml de Cadastro.java

---
+++ src/main/java/view/Cadastro.java
@@ -0,0 +1,45 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class Cadastro  extends Application {
+
+    @FXML
+    private TextField emailField;
+    private String caminhoCss;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Cadastro.fxml"));
+        Parent root = loader.load();
+
+
+        // Inicializar o controller após carregar o FXML
+        Cadastro controller = loader.getController();
+
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Cadastro InventarioHGV");
+
+        root.requestFocus();
+        stage.show();
+
+
+    }
+
+
+
+    private void registrar(){
+
+    }
+
+
+}
