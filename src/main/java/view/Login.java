b0791cd5f28515ccc660cfab0f6568f28cc522d4
jpvasconcelos-m <joaopedrosao@gmail.com>
2024-02-25 23:26:06 -0300

    implementeção de listener

--- src/main/java/view/Login.java
+++ src/main/java/view/Login.java
@@ -2,6 +2,9 @@ package view;
 
 import controller.AutenticadorLogin;
 import javafx.application.Application;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,8 +14,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javassist.Loader;
 
 import java.io.IOException;
+import java.util.EventListener;
 
 public class Login extends Application {
 
@@ -32,13 +37,66 @@ public class Login extends Application {
     FXMLControllerUtils fxmlControllerUtils = new FXMLControllerUtils();
     boolean autenticou;
 
+    SimpleBooleanProperty booleanProperty = new SimpleBooleanProperty();
+
+    public ChangeListener getChangeListener() {
+        return changeListener;
+    }
+
+    public Login(Main main){
+        this.main = main;
+        booleanProperty.addListener(changeListener);
+
+
+
+    }
+    public Login(){
+
+    }
+
+
+    public Main getMain() {
+        return main;
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+
+    }
+    private Main main;
+
+    private ChangeListener changeListener= new ChangeListener() {
+        @Override
+        public void changed(ObservableValue observableValue, Object o, Object t1) {
+            System.out.println("valor mudou");
+        }
+    };
+
+   {
+       if((booleanProperty.getValue()==true) && (this.getMain()!=null)){
+           booleanProperty.addListener(this.getMain().getChangeListener());
+
+           this.getMain().getChangeListener().changed(booleanProperty,false,true);
+       }
+       else {
+           System.out.println("problema");
+       }
+
+    }
+
+
+
 
 
     @Override
     public void start(Stage stage) throws Exception {
 
+        chamarCenaLogin(this.main);
+
+
+
         stage.setTitle("InventarioHGV");
-        stage.setScene(chamarCenaLogin());
+        stage.setScene(chamarCenaLogin( ));
         //Adiciona icone padrão da aplicação
         fxmlControllerUtils.adicionarIconeHGV(stage);
 
@@ -51,35 +109,62 @@ public class Login extends Application {
 
 
     public static void main(String[] args) {
+
+
         launch(args);
     }
 
     @FXML
-    private boolean entrarUsuario(ActionEvent event) throws IOException {
+    private void entrarUsuario(ActionEvent event) throws IOException {
+
+       main = new Main();
+
+        System.out.println(this.getMain());
         System.out.println("botao entrar pressionado!");
 
-       boolean autenticou = AutenticadorLogin.autenticar(loginField.getText(),passwordField.getText());
+
+
+
+
+
+       autenticou = AutenticadorLogin.autenticar(loginField.getText(),passwordField.getText());
         System.out.println(autenticou);
 
-        //Retorna se o login ocorreu ou não.
-        return autenticou;
+        changeListener.changed(booleanProperty,false,autenticou);
+
+
+
+        booleanProperty.set(autenticou);
+        System.out.println(booleanProperty);
+
+
+
+
 
     }
 
 
-    public Scene chamarCenaLogin() throws IOException {
+    public Scene chamarCenaLogin(Main main) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
         Scene scene = new Scene(root);
 
 
 
+        System.out.println(main);
+        return scene;
 
+
+    } public Scene chamarCenaLogin() throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
+        Scene scene = new Scene(root);
 
 
         return scene;
 
 
     }
+
 
 
 
--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -1,27 +1,48 @@
 package view;
 
 import javafx.application.Application;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
 
+    public ChangeListener getChangeListener() {
+        return changeListener;
+    }
+
+    public void setChangeListener(ChangeListener changeListener) {
+        this.changeListener = changeListener;
+    }
+
+     ChangeListener changeListener= new ChangeListener() {
+        @Override
+        public void changed(ObservableValue observableValue, Object o, Object t1) {
+            System.out.println("valor mudou main");
+        }
+    };
+
+
 
     @Override
     public void start(Stage stage) throws Exception {
-        Login login = new Login();
-        Cadastro cadastro = new Cadastro();
-        stage.setScene(login.chamarCenaLogin());
+
+
+
+        Login login = new Login(this);
+        login.booleanProperty.addListener(this.changeListener);
+
+
+        login.booleanProperty.addListener((observableValue, aBoolean, t1) -> System.out.println("aaaaaa"));
+        stage.setScene(login.chamarCenaLogin(this));
 
 
         stage.show();
 
 
-
-
-
-
-
-
     }
+
+
 }
