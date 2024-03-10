267317de6b5ec7f6624bde0efb41fc82568a1926
jpvasconcelos-m <joaopedrosao@gmail.com>
2024-03-10 18:04:37 -0300

    adicionado comentário

--- src/main/java/model/HashSenhas.java
+++ src/main/java/model/HashSenhas.java
@@ -4,6 +4,8 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class HashSenhas {
+
+    //Classe do spring security para geração de salts e hashs.
     private static BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public String generateSalt(){
